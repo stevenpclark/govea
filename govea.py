ab3825bc6e15a341f3bf4be7a35e1213c0141d83
Steven Clark <steven.p.clark@gmail.com>
2015-07-27 21:24:22 -0400

    govea.py rank comparison code

--- govea.py
+++ govea.py
@@ -8,11 +8,33 @@ str_to_color = {'B':B, 'W':W}
 def opposite_color(c):
 	return -c
 
+def rank_str_to_num(s):
+	"""input str is either <n>k, <n>d, or <n>p.
+	e.g. 23k, 3d, 9p.
+	<n>k -> -n
+	<n>d -> n
+	<n>p -> n+10
+	This is somewhat subjective, but lets us at least compare ranks."""
+	if s is None:
+		return -35 #TODO: what to do about unspecified ranks?
+	n = int(s[0:-1])
+	c = s[-1]
+	if c == 'k':
+		return -n
+	elif c == 'd':
+		return n
+	elif c == 'p':
+		return n+10
+	else:
+		raise ValueError('rank final char needs to be one of [k,d,p]')
+
+
 class Player(object):
-	def __init__(self, color, name=None, rank=None):
+	def __init__(self, color, name=None, rank_str=None):
 		self.color = color
 		self.name = name
-		self.rank = rank
+		self.rank_str = rank_str
+		self.rank_num = rank_str_to_num(rank_str)
 
 	def __repr__(self):
 		return '%s: %s [%s]' % (color_to_str[self.color], self.name, self.rank)
